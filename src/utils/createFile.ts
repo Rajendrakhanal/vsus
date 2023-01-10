c46ccd953d68390b00aa395fad2bb8bdd3943f40
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-10 22:57:10 +0545

    Update createFile function to render the passed content in the file created

--- src/utils/createFile.ts
+++ src/utils/createFile.ts
@@ -1,9 +1,13 @@
 import * as path from "path";
 import * as fse from "fs-extra";
 
-export function createFile(location: string, fileName: string): void {
+export function createFile(
+  location: string,
+  fileName: string,
+  content: string
+): void {
   try {
-    fse.ensureFileSync(path.join(location, fileName));
+    fse.writeFileSync(path.join(location, fileName), content);
   } catch (error) {
     console.log(error);
   }
