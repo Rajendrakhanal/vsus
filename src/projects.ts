4d92b79146c086360090c4c5e246b9ed182e6949
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-11 14:07:26 +0545

    Typescript file to create Nodejs Project

--- src/projects.ts
+++ src/projects.ts
@@ -3,15 +3,21 @@
 import * as vscode from "vscode";
 import { openDialogBox } from "./utils/openDialogBox";
 import { cppProject } from "./projects/cpp";
+import { nodeProject } from "./projects/node";
 
 export async function createProject(fileType: string) {
   const folderName = await openDialogBox();
 
+  console.log(folderName);
   if (folderName && folderName.fsPath) {
     switch (fileType) {
       case "cpp":
         await vscode.commands.executeCommand("vscode.openFolder", folderName);
         await cppProject.create(folderName.fsPath);
+        break;
+      case "node":
+        await vscode.commands.executeCommand("vscode.openFolder", folderName);
+        await nodeProject.create(folderName.fsPath);
         break;
 
       default:
