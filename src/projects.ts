d526bc7b08bbbfa80dbfc70b56a6a1057730ba26
Rajendra Khanal <khanal.rajendra111@gmail.com>
2023-01-11 12:57:19 +0545

    Added create react project command

--- src/projects.ts
+++ src/projects.ts
@@ -3,6 +3,7 @@
 import * as vscode from "vscode";
 import { openDialogBox } from "./utils/openDialogBox";
 import { cppProject } from "./projects/cpp";
+import { reactProject } from "./projects/react";
 
 export async function createProject(fileType: string) {
   const folderName = await openDialogBox();
@@ -12,6 +13,10 @@ export async function createProject(fileType: string) {
       case "cpp":
         await vscode.commands.executeCommand("vscode.openFolder", folderName);
         await cppProject.create(folderName.fsPath);
+        break;
+
+      case "react":
+        await reactProject.createReactProject(folderName.fsPath);
         break;
 
       default:
--- src/projects/react.ts
+++ src/projects/react.ts
@@ -1,3 +1,17 @@
-/**
- * @Rajendra
- */
+import * as vscode from "vscode";
+export namespace reactProject {
+  export const createReactProject = async (destination: string) => {
+    const projectName = await vscode.window.showInputBox({
+      ignoreFocusOut: true,
+      placeHolder: "Name of your project. Example: hello-world",
+      prompt: "Enter name of your project",
+      validateInput: (text: string) => {
+        const correctProjectName = /^[A-Za-z-]+$/;
+        if (text.match(correctProjectName)) {
+          return null;
+        }
+        return "Error project name.";
+      },
+    });
+  };
+}
