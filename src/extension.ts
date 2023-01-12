76e2d0d78380e16a325b6b5a31646efe1fc354ae
Rajendra Khanal <khanal.rajendra111@gmail.com>
2023-01-12 11:39:45 +0545

    register command for askASUS is successful

--- src/extension.ts
+++ src/extension.ts
@@ -11,6 +11,20 @@ export function activate(context: vscode.ExtensionContext) {
     console.log(vscode.window.activeTextEditor?.document.fileName);
   });
 
+  context.subscriptions.push(
+    vscode.commands.registerCommand("askVSUS.start", () => {
+      const panel = vscode.window.createWebviewPanel(
+        "AskVsus", // Identifies the type of the webview. Used internally
+        "AskVsus", // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+      );
+      // And set its HTML content
+     // panel.webview.html = getWebviewContent();
+    })
+  );
+
+
   const c = getLanguageCompletion("c");
   const cpp = getLanguageCompletion("cpp");
   const java = getLanguageCompletion("java");
