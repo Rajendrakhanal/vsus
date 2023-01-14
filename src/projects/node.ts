8682f02174ef48f794731f7082261ddf470193d4
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-14 20:54:00 +0545

    Fix: Temporary fix the issue of project creation of nodejs
    Terminal command were not running

--- src/projects/node.ts
+++ src/projects/node.ts
@@ -5,11 +5,19 @@ import { createFile } from "../utils/createFile";
 import { createFolder } from "../utils/createFolder";
 
 import * as starterFile from "./staterContent.json";
+import { showInputBox } from "../utils/showInputBox";
 
 export namespace nodeProject {
   const projectDetails = starterFile.node;
 
-  export const create = (destination: string) => {
+  export const create = async(destination: string) => {
+
+    const projectName = await showInputBox(
+      "Name of your project. Example: hello-world",
+      "Enter name of your project"
+    );
+    
+    destination = destination + "/" +projectName;
     projectDetails.dir.forEach((folderName: string) => {
       try {
         createFolder(destination, folderName);
